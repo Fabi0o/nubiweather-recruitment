00d77b0d1c7f5de13d6f2b9c64cac97e9ef604d2
Fabi0o <marfab32@gmail.com>
2024-09-23 19:51:51 +0200

    fix: makes homepage responsive

--- full-stack-solution/app/page.tsx
+++ full-stack-solution/app/page.tsx
@@ -2,18 +2,16 @@ import WeatherCard from "@/components/WeatherCard";
 import { ForecastWeatherRes, RealtimeWeatherRes } from "@/types/weatherInfo";
 
 export default async function Home() {
-  const currentWeatherData = await fetch(
-    "http://localhost:3000/realtime-weather"
-  );
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || process.env.VERCEL_URL;
+
+  const currentWeatherData = await fetch(`${baseUrl}/realtime-weather`);
   const currentWeather: RealtimeWeatherRes = await currentWeatherData.json();
 
-  const forecastWeatherData = await fetch(
-    "http://localhost:3000/forecast-weather/3"
-  );
+  const forecastWeatherData = await fetch(` ${baseUrl}/forecast-weather/3`);
   const forecastWeather: ForecastWeatherRes = await forecastWeatherData.json();
 
   return (
-    <div className=" flex justify-center items-center h-screen gap-3">
+    <div className=" flex flex-col justify-center items-center h-screen gap-3 md:flex-row">
       <WeatherCard
         currentWeatherInfo={currentWeather.weatherGliwice}
         forecastWeatherInfo={forecastWeather.forecastWeatherGliwice}
